39e78f934218391b89efa10bfd080b80a092fbb4
Matheus Silva <103164463+MSOliver1998@users.noreply.github.com>
2022-10-18 18:10:45 -0300

    first commit

---
+++ src/scripts/database.js
@@ -0,0 +1,47 @@
+const pets = [
+  {
+    nome: "Beatriz",
+    raca: "Poodle",
+    tomouBanho: true,
+  },
+  {
+    nome: "Tron",
+    raca: "Pitbull",
+    tomouBanho: false,
+  },
+  {
+    nome: "Bela",
+    raca: "Poodle",
+    tomouBanho: false,
+  },
+  {
+    nome: "Mittens",
+    raca: "Poodle",
+    tomouBanho: false,
+  },
+  {
+    nome: "Duquesa",
+    raca: "Pug",
+    tomouBanho: false,
+  },
+  {
+    nome: "Caos",
+    raca: "Rottweiler",
+    tomouBanho: true,
+  },
+  {
+    nome: "Maya",
+    raca: "Poodle",
+    tomouBanho: true,
+  },
+  {
+    nome: "Luna",
+    raca: "Poodle",
+    tomouBanho: true,
+  },
+  {
+    nome: "Bartolomeu",
+    raca: "Pinscher",
+    tomouBanho: true,
+  },
+];
---
+++ src/scripts/index.js
@@ -0,0 +1,31 @@
+function click(){
+    let button=document.querySelector('.render-button')
+    button.addEventListener('click',renderPets)
+}
+
+function renderPets(){
+    let ul=document.querySelector('.container-cards')
+    cont=0
+
+    pets.forEach(el=>{
+        if (el.tomouBanho==true && el.raca=="Poodle"){
+            cont++
+        }
+        let card= document.createElement('li')
+        let nome=document.createElement('h2')
+        let raca=document.createElement('p')
+
+        card.classList.add('card')
+        nome.innerText=el.nome
+        raca.innerText=el.raca
+
+        card.append(nome,raca)
+        ul.append(card)
+
+    })
+    let contador=document.querySelector('.span-counter')
+    
+    console.log(cont)
+}
+
+click()
